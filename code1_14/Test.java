6c47dbccd5858f1f0eb86baa9f599cb8f7a14fec
宁鹏 <552432022@qq.com>
2019-01-14 23:54:40 +0800

    Happy Birthday ,YU

---
+++ code1_14/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("2019.01.14");
+        System.out.println("小可爱生日快乐");
+        System.out.println("Happy Birthday to Yu");
+
+        
+    }
+} 
