825202ff77fe18033cbc596dd8fe54a5461c2429
宁鹏 <552432022@qq.com>
2019-02-02 23:04:35 +0800

    定义一个人类，一个学生类

---
+++ code2_2/Test.java
@@ -0,0 +1,56 @@
+//定义两个类（人类，学生类）
+class Person{
+    private String name;
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+}
+
+class Student{
+    private String name;
+    private int age;
+    private String school;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getSchool(){
+        return school;
+    }
+    public void setSchool(String school){
+        this.school = school;
+    }
+}
+
+public class Test{
+    public static void main(String[] args){
+
+    }
+}
