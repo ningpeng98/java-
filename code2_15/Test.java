a499a495432e5fc40bbfa5ad5eb0f5e9e7c5683c
宁鹏 <552432022@qq.com>
2019-02-15 21:50:25 +0800

    定义一个方法，该方法可以接收Person类的所有子类实例，并调用Person类的方法

---
+++ code2_15/Test.java
@@ -0,0 +1,27 @@
+class Person{
+    public void print(){
+        System.out.println("1.我是人类！");
+    }
+}
+
+class Student extends Person{
+    public void print(){
+        System.out.println("2.我是学生！");
+    }
+}
+
+class Worker extends Person{
+    public void print(){
+        System.out.println("3.我是工人");
+    }
+}
+public class Test{
+    public static void main(String[] args){
+        whoYouAre(new Student());
+        whoYouAre(new Worker());
+    }
+    public static void whoYouAre(Person per){
+        per.print();
+    }
+}
+
