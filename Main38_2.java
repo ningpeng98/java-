b084efcdcc89ab3a780f33c9598be22fbddceca4
ningpeng98 <552432022@qq.com>
2019-07-18 16:22:06 +0800

    //牛客#空格替换

---
+++ Main38_2.java
@@ -0,0 +1,11 @@
+package Day38;
+//牛客#空格替换
+import java.util.*;
+/*
+public class Replacement {
+    public String replaceSpace(String iniString, int length) {
+        // write code here
+        return iniString.replaceAll(" ","%20");
+    }
+}
+*/
