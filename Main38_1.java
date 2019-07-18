5b565da2b86aefadeb915d443d00a5a68cea052b
ningpeng98 <552432022@qq.com>
2019-07-18 16:21:45 +0800

    //牛客#最近公共祖先

---
+++ Main38_1.java
@@ -0,0 +1,25 @@
+package Day38;
+//牛客#最近公共祖先
+import java.util.*;
+/*
+public class LCA {
+    public int getLCA(int a, int b) {
+        // write code here
+        if(a==1||a==1){
+            return 1;
+        }
+        return sel(a,b);
+    }
+    public static int sel(int a,int b){
+        while(a!=b){
+            if(a>b){
+                a=a/2;
+            }
+            else{
+                b=b/2;
+            }
+        }
+        return a;
+    }
+}
+*/
