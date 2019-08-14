a5384c0f0ed0f520f9fac9a9f8803157ac21ad0a
ningpeng98 <552432022@qq.com>
2019-08-14 23:53:50 +0800

    项目：性能测试框架

---
+++ benchmarkFinal/src/main/java/com/test/cases/AddCast.java
@@ -0,0 +1,43 @@
+package com.test.cases;
+
+
+import com.test.Case;
+import com.test.annotations.Benchmark;
+import com.test.annotations.Measurement;
+
+/**
+ *
+ *  String的"+"操作与StringBuilder的append()方法对比
+ * */
+@Measurement(iterations = 10,group = 3)
+public class AddCast  {
+    private static String StringAdd(){
+        String s = "";
+        for (int i = 0; i < 10; i++) {
+            s+=i;
+        }
+        return s;
+    }
+    private static String StringBuilderAdd(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+
+    @Benchmark
+    public void testStringAdd(){
+        StringAdd();
+
+    }
+
+
+    @Benchmark
+    public void testStringBuilderAdd(){
+        StringBuilderAdd();
+    }
+
+
+
+}
